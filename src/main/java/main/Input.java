627e68d5b638b1081351a439803998741ee03478
JunoJin <jjin525@aucklanduni.ac.nz>
2017-07-05 14:21:02 +1200

    didn;t commit Input for some reason

---
+++ src/main/java/main/Input.java
@@ -0,0 +1,51 @@
+package main;
+
+import org.gephi.graph.api.DirectedGraph;
+import org.gephi.graph.api.GraphController;
+import org.gephi.graph.api.GraphModel;
+import org.gephi.io.importer.api.Container;
+import org.gephi.io.importer.api.EdgeDirectionDefault;
+import org.gephi.io.importer.api.ImportController;
+import org.gephi.io.processor.plugin.DefaultProcessor;
+import org.gephi.project.api.ProjectController;
+import org.gephi.project.api.Workspace;
+import org.openide.util.Lookup;
+
+import java.io.File;
+
+/**
+ * Created by Juno on 4/07/2017.
+ */
+public class Input {
+    private ProjectController pc;
+    private Workspace workspace;
+
+    public Input(){
+        pc = Lookup.getDefault().lookup(ProjectController.class);
+        pc.newProject();
+        workspace = pc.getCurrentWorkspace();
+    }
+
+    public DirectedGraph Import(String fileName){
+        ImportController importController = Lookup.getDefault().lookup(ImportController.class);
+        Container container;
+
+        try{
+            File file = new File(getClass().getResource(fileName).toURI());
+            container = importController.importFile(file);
+            container.getLoader().setEdgeDefault(EdgeDirectionDefault.DIRECTED);
+            container.getLoader().setAllowAutoNode(false);
+        } catch(Exception ex){
+            ex.printStackTrace();
+            return null;
+        }
+
+        importController.process(container,new DefaultProcessor(), workspace);
+
+        GraphModel graphModel = Lookup.getDefault().lookup(GraphController.class).getGraphModel();
+        DirectedGraph graph = graphModel.getDirectedGraph();
+        System.out.println("Nodes: " + graph.getNodeCount());
+        System.out.println("Edges: " + graph.getEdgeCount());
+        return graph;
+    }
+}
