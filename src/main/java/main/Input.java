ab3ff6a2e0ddd25f95f0ee419b6fb43dcc963905
JunoJin <jjin525@aucklanduni.ac.nz>
2017-07-11 19:06:52 +1200

    now import returns a facNodeList and popNodeList population changed to populationScore as they only represent potential, not actual population

--- src/main/java/main/Input.java
+++ src/main/java/main/Input.java
@@ -1,8 +1,6 @@
 package main;
 
-import org.gephi.graph.api.DirectedGraph;
-import org.gephi.graph.api.GraphController;
-import org.gephi.graph.api.GraphModel;
+import org.gephi.graph.api.*;
 import org.gephi.io.importer.api.Container;
 import org.gephi.io.importer.api.EdgeDirectionDefault;
 import org.gephi.io.importer.api.ImportController;
@@ -12,6 +10,8 @@ import org.gephi.project.api.Workspace;
 import org.openide.util.Lookup;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Juno on 4/07/2017.
@@ -26,14 +26,15 @@ public class Input {
         workspace = pc.getCurrentWorkspace();
     }
 
-    public DirectedGraph Import(String fileName){
+    public NodeListHolder Import(String fileName){
+        NodeListHolder nlh = new NodeListHolder();
         ImportController importController = Lookup.getDefault().lookup(ImportController.class);
         Container container;
 
         try{
             File file = new File(getClass().getResource(fileName).toURI());
             container = importController.importFile(file);
-            container.getLoader().setEdgeDefault(EdgeDirectionDefault.DIRECTED);
+            container.getLoader().setEdgeDefault(EdgeDirectionDefault.UNDIRECTED);
             container.getLoader().setAllowAutoNode(false);
         } catch(Exception ex){
             ex.printStackTrace();
@@ -46,6 +47,88 @@ public class Input {
         DirectedGraph graph = graphModel.getDirectedGraph();
         System.out.println("Nodes: " + graph.getNodeCount());
         System.out.println("Edges: " + graph.getEdgeCount());
-        return graph;
+
+        List<PopNode> popNodeList = new ArrayList<>();
+        List<FacNode> facNodeList = new ArrayList<>();
+
+        List<String> zones = new ArrayList<>();
+        for (org.gephi.graph.api.Node n: graph.getNodes()) {
+            String label = n.getLabel();
+            String[] nodeLabels = label.split(";");
+            if (nodeLabels[1].startsWith("Residential")){
+                float populationScore = CalculatePopulationScore(nodeLabels[2], Float.valueOf(nodeLabels[5]));
+                popNodeList.add(new PopNode(populationScore,n.x(),n.y()));
+            } else if (nodeLabels[1].startsWith("Business")){
+                facNodeList.add(new FacNode(true, n.x(),n.y()));
+            }
+        }
+        nlh.setNodeLists(popNodeList, facNodeList);
+
+        return nlh;
+    }
+
+    class NodeListHolder{
+        private List<PopNode> popNodeList = new ArrayList<>();
+        private List<FacNode> facNodeList = new ArrayList<>();
+
+        private void setNodeLists(List<PopNode> popNodeList, List<FacNode> facNodeList){
+            this.popNodeList = popNodeList;
+            this.facNodeList = facNodeList;
+        }
+
+        public List<PopNode> getPopNodeList() {
+            return popNodeList;
+        }
+
+        public List<FacNode> getFacNodeList() {
+            return facNodeList;
+        }
+    }
+
+    public float CalculatePopulationScore(String zone, float area){
+        int density;
+        switch (zone) {
+            case "1":
+                density = 1200;
+                break;
+            case "2A1":
+                density = 5000;
+                break;
+            case "2A":
+                density = 800;
+                break;
+            case "2B":
+            case "3C":
+                density = 600;
+                break;
+            case "2C":
+                density = 350;
+                break;
+            case "3A":
+            case "4A":
+            case "4B":
+                density = 450;
+                break;
+            case "3B":
+                density = 500;
+                break;
+            case "5":
+                density = 350;
+                break;
+            case "6A":
+            case "6A1":
+            case "6B":
+            case "6B1":
+            case "6C":
+            case "6C1":
+                density = 150;
+                break;
+            case "7":
+                density = 200;
+                break;
+            default:
+                density = 0;
+        }
+        return (area/density);
     }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,35 +8,35 @@ import org.jgrapht.graph.SimpleWeightedGraph;
 
 public class Main {
 	public static void main(String[] args){
-		
-		SimpleWeightedGraph<Node, DefaultWeightedEdge> testGraph = new SimpleWeightedGraph<Node, DefaultWeightedEdge>(DefaultWeightedEdge.class);
-		List<PopNode> popNodeList = new ArrayList<>();
-		List<FacNode> facNodeList = new ArrayList<>();
-		
-		//Nodes
-		FacNode n1 = new FacNode(true,2,2);
-		FacNode n2 = new FacNode(true,3,5);
-		PopNode n3 = new PopNode(2,1,1);
-		PopNode n4 = new PopNode(10,4,4);
-		
-		facNodeList.add(n1);
-		facNodeList.add(n2);
-		popNodeList.add(n3);
-		popNodeList.add(n4);
-		
-		//Adding Nodes
-		testGraph.addVertex(n1);
-		testGraph.addVertex(n2);
-		testGraph.addVertex(n3);
-		testGraph.addVertex(n4);
+//
+//		SimpleWeightedGraph<Node, DefaultWeightedEdge> testGraph = new SimpleWeightedGraph<Node, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+//		List<PopNode> popNodeList = new ArrayList<>();
+//		List<FacNode> facNodeList = new ArrayList<>();
+//
+//		//Nodes
+//		FacNode n1 = new FacNode(true,2,2);
+//		FacNode n2 = new FacNode(true,3,5);
+//		PopNode n3 = new PopNode(2,1,1);
+//		PopNode n4 = new PopNode(10,4,4);
+//
+//		facNodeList.add(n1);
+//		facNodeList.add(n2);
+//		popNodeList.add(n3);
+//		popNodeList.add(n4);
+//
+//		//Adding Nodes
+//		testGraph.addVertex(n1);
+//		testGraph.addVertex(n2);
+//		testGraph.addVertex(n3);
+//		testGraph.addVertex(n4);
+//
+//		FacilityAlgorithm fa = new FacilityAlgorithm();
+//		Node bestNode = fa.Solve(facNodeList,popNodeList);
+//
+//		System.out.println(bestNode.getX() + " " + bestNode.getY());
 
-		FacilityAlgorithm fa = new FacilityAlgorithm();
-		Node bestNode = fa.Solve(facNodeList,popNodeList);
-		
-		System.out.println(bestNode.getX() + " " + bestNode.getY());
-
-//		Input i = new Input();
-//		org.gephi.graph.api.DirectedGraph graph = i.Import("300m.gml");
+		Input i = new Input();
+		Input.NodeListHolder nlh = i.Import("300m.gml");
 	}
 }
 
@@ -54,16 +54,16 @@ class Node {
 }
 
 class PopNode extends Node {
-	private int population;
+	private float populationScore;
 	
-	public PopNode(int population, float xCoord, float yCoord){
-		this.population = population;
+	public PopNode(float populationScore, float xCoord, float yCoord){
+		this.populationScore = populationScore;
 		this.xCoord = xCoord;
 		this.yCoord = yCoord;
 	}
 	
-	public int getPopulation(){
-		return population;
+	public float getPopulationScore(){
+		return populationScore;
 	}
 }
 
