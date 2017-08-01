3ee55693e64e24cf84599c5b235a91d973eeb4e6
Nathan Situ <nsit984@UoA.auckland.ac.nz>
2017-08-01 16:56:20 +1200

    Update greedy algorithm

--- src/main/java/main/LocalSearchWithSwap.java
+++ src/main/java/main/LocalSearchWithSwap.java
@@ -1,5 +1,28 @@
 package main;
 
-public class LocalSearchWithSwap {
+import java.util.List;
 
+import org.gephi.graph.api.Graph;
+
+public class LocalSearchWithSwap {
+	public static List<Node> Search(Graph graph, int facCount){
+		
+		//Initial K
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		//Swap
+		//Assuming it has the intitial facility nodes
+		
+		
+		
+		return null;
+		
+	}
 }
