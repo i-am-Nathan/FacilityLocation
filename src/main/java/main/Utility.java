af4fa9897d744a307734e0ff4834c88b8395bfb1
i-am-Nathan <nathansitu@hotmail.com>
2017-08-17 00:19:36 +1200

    Update Utility to have population densities

--- src/main/java/main/Utility.java
+++ src/main/java/main/Utility.java
@@ -37,38 +37,36 @@ import java.util.List;
 
 public class Utility {
 	static int euclidDistance(FacNode fac1, FacNode fac2) {
-		Double distance = Math.sqrt(Math.pow(fac2.xCoord-fac1.xCoord,2) + Math.pow(fac2.yCoord - fac1.yCoord, 2));
+		Double distance = Math.sqrt(Math.pow(fac2.xCoord - fac1.xCoord, 2) + Math.pow(fac2.yCoord - fac1.yCoord, 2));
 		return (int) Math.round(distance);
 	}
 
-
 	@SuppressWarnings("unchecked")
-	public static HashMap sortByValues(HashMap map){
+	public static HashMap sortByValues(HashMap map) {
 		List list = new LinkedList(map.entrySet());
 
-		Collections.sort(list, new Comparator(){
+		Collections.sort(list, new Comparator() {
 
 			@Override
 			public int compare(Object o1, Object o2) {
-				return((Comparable)((Map.Entry)(o1)).getValue()).compareTo(((Map.Entry)(o2)).getValue());
+				return ((Comparable) ((Map.Entry) (o1)).getValue()).compareTo(((Map.Entry) (o2)).getValue());
 			}
 
 		});
 
 		HashMap sortedHashMap = new LinkedHashMap();
-		for(Iterator it = list.iterator(); it.hasNext();){
-			Map.Entry entry = (Map.Entry)it.next();
+		for (Iterator it = list.iterator(); it.hasNext();) {
+			Map.Entry entry = (Map.Entry) it.next();
 			sortedHashMap.put(entry.getKey(), entry.getValue());
 		}
-
 
 		return sortedHashMap;
 
 	}
 
-	static List<List<Node>> findInitialK(Graph graph, int k, float coverageThreshold, String centralityType){
-	    List<List<Node>> nodeLists = new ArrayList<>();
-	    List<org.gephi.graph.api.Node> kNodeList = new ArrayList<>();
+	static List<List<Node>> findInitialK(Graph graph, int k, float coverageThreshold, String centralityType) {
+		List<List<Node>> nodeLists = new ArrayList<>();
+		List<org.gephi.graph.api.Node> kNodeList = new ArrayList<>();
 
 		FilterController filterController = Lookup.getDefault().lookup(FilterController.class);
 
@@ -82,9 +80,11 @@ public class Utility {
 
 		UndirectedGraph cleanGraph = graph.getModel().getUndirectedGraph(graphView);
 
-        AttributeColumnsController attributeColumnsController = Lookup.getDefault().lookup(AttributeColumnsController.class);
-        Table edgeTable = cleanGraph.getModel().getEdgeTable();
-        attributeColumnsController.copyColumnDataToOtherColumn(edgeTable, edgeTable.getColumn("Label"), edgeTable.getColumn("Weight"));
+		AttributeColumnsController attributeColumnsController = Lookup.getDefault()
+				.lookup(AttributeColumnsController.class);
+		Table edgeTable = cleanGraph.getModel().getEdgeTable();
+		attributeColumnsController.copyColumnDataToOtherColumn(edgeTable, edgeTable.getColumn("Label"),
+				edgeTable.getColumn("Weight"));
 
 		Modularity modularity = new Modularity();
 		modularity.setUseWeight(true);
@@ -92,76 +92,125 @@ public class Utility {
 
 		float totalCoverage = 0;
 		int resolution = 1;
-        Column classColumn = null;
-        Object[] percentages = null;
+		Column classColumn = null;
+		Object[] percentages = null;
 
-		while(totalCoverage<coverageThreshold){
-		    totalCoverage = 0;
+		while (totalCoverage < coverageThreshold) {
+			totalCoverage = 0;
 			modularity.setResolution(resolution);
 			modularity.execute(cleanGraph);
 
 			classColumn = cleanGraph.getModel().getNodeTable().getColumn(Modularity.MODULARITY_CLASS);
-			Function func2 = appearanceModel.getNodeFunction(graph, classColumn, PartitionElementColorTransformer.class);
+			Function func2 = appearanceModel.getNodeFunction(graph, classColumn,
+					PartitionElementColorTransformer.class);
 			Partition partition2 = ((PartitionFunction) func2).getPartition();
 			System.out.println(partition2.size() + " communities found");
 			percentages = partition2.getSortedValues().toArray();
 
-			for(int classIndex = 0; classIndex < k; classIndex++){
-			    System.out.println("Community "+classIndex+": "+partition2.percentage(percentages[classIndex]));
-			    totalCoverage += partition2.percentage(percentages[classIndex]);
-            }
+			for (int classIndex = 0; classIndex < k; classIndex++) {
+				System.out.println("Community " + classIndex + ": " + partition2.percentage(percentages[classIndex]));
+				totalCoverage += partition2.percentage(percentages[classIndex]);
+			}
 			resolution++;
-            System.out.println("Total coverage over " + k + " communities: " + totalCoverage + "%\n");
+			System.out.println("Total coverage over " + k + " communities: " + totalCoverage + "%\n");
 		}
 
-		System.out.println("#### Total coverage now over "+coverageThreshold+"%!\n\n");
+		System.out.println("#### Total coverage now over " + coverageThreshold + "%!\n\n");
 
-        PartitionBuilder.PartitionFilter partitionFilter = new PartitionBuilder.NodePartitionFilter(classColumn, appearanceModel);
-        for(int classIndex = 0; classIndex < k; classIndex++) {
-            List<Node> communityNodeList = new ArrayList<>();
-            partitionFilter.addPart(percentages[classIndex]);
-            Query query1 = filterController.createQuery(partitionFilter);
-            filterController.setSubQuery(query1, query);
-            GraphView communityView = filterController.filter(query1);
-            UndirectedGraph communityGraph = graph.getModel().getUndirectedGraph(communityView);
+		PartitionBuilder.PartitionFilter partitionFilter = new PartitionBuilder.NodePartitionFilter(classColumn,
+				appearanceModel);
+		for (int classIndex = 0; classIndex < k; classIndex++) {
+			List<Node> communityNodeList = new ArrayList<>();
+			partitionFilter.addPart(percentages[classIndex]);
+			Query query1 = filterController.createQuery(partitionFilter);
+			filterController.setSubQuery(query1, query);
+			GraphView communityView = filterController.filter(query1);
+			UndirectedGraph communityGraph = graph.getModel().getUndirectedGraph(communityView);
 
-            EigenvectorCentrality eigenvectorCentrality = new EigenvectorCentrality();
-            eigenvectorCentrality.execute(communityGraph);
+			EigenvectorCentrality eigenvectorCentrality = new EigenvectorCentrality();
+			eigenvectorCentrality.execute(communityGraph);
 
-            GraphDistance graphDistance = new GraphDistance();
-            graphDistance.execute(communityGraph);
-            double maxCentrality = 0;
-            org.gephi.graph.api.Node maxCentralityNode = null;
+			GraphDistance graphDistance = new GraphDistance();
+			graphDistance.execute(communityGraph);
+			double maxCentrality = 0;
+			org.gephi.graph.api.Node maxCentralityNode = null;
 
-            System.out.println("Nodes in community " + classIndex + ": " + communityGraph.getNodeCount());
-            System.out.println("Edges in community " + classIndex + ": " + communityGraph.getEdgeCount());
+			System.out.println("Nodes in community " + classIndex + ": " + communityGraph.getNodeCount());
+			System.out.println("Edges in community " + classIndex + ": " + communityGraph.getEdgeCount());
 
-            Column centralityColumn = null;
+			Column centralityColumn = null;
 
-            for(org.gephi.graph.api.Node node:communityGraph.getNodes()){
-                communityNodeList.add(node);
-                if(!node.getLabel().contains("Business")) continue;
-                for(Column col: node.getAttributeColumns()){
-                    if(col.getTitle().equals(centralityType)){
-                        centralityColumn = col;
-                        break;
-                    }
-                }
-                double nodeCentrality = (double)node.getAttribute(centralityColumn);
-                if(nodeCentrality > maxCentrality) {
-                    maxCentralityNode = node;
-                    maxCentrality = nodeCentrality;
-                }
-            }
-            kNodeList.add(maxCentralityNode);
-            nodeLists.add(communityNodeList);
-            partitionFilter.removePart(percentages[classIndex]);
-        }
+			for (org.gephi.graph.api.Node node : communityGraph.getNodes()) {
+				communityNodeList.add(node);
+				if (!node.getLabel().contains("Business"))
+					continue;
+				for (Column col : node.getAttributeColumns()) {
+					if (col.getTitle().equals(centralityType)) {
+						centralityColumn = col;
+						break;
+					}
+				}
+				double nodeCentrality = (double) node.getAttribute(centralityColumn);
+				if (nodeCentrality > maxCentrality) {
+					maxCentralityNode = node;
+					maxCentrality = nodeCentrality;
+				}
+			}
+			kNodeList.add(maxCentralityNode);
+			nodeLists.add(communityNodeList);
+			partitionFilter.removePart(percentages[classIndex]);
+		}
 
-        nodeLists.add(kNodeList);
+		nodeLists.add(kNodeList);
 
 		return nodeLists;
 
 	}
 
+	public static float CalculatePopulationScore(String zone, float area) {
+		int density;
+		switch (zone) {
+		case "1":
+			density = 1200;
+			break;
+		case "2A1":
+			density = 5000;
+			break;
+		case "2A":
+			density = 800;
+			break;
+		case "2B":
+		case "3C":
+			density = 600;
+			break;
+		case "2C":
+			density = 350;
+			break;
+		case "3A":
+		case "4A":
+		case "4B":
+			density = 450;
+			break;
+		case "3B":
+			density = 500;
+			break;
+		case "5":
+			density = 350;
+			break;
+		case "6A":
+		case "6A1":
+		case "6B":
+		case "6B1":
+		case "6C":
+		case "6C1":
+			density = 150;
+			break;
+		case "7":
+			density = 200;
+			break;
+		default:
+			return 0;
+		}
+		return (area / density);
+	}
 }
