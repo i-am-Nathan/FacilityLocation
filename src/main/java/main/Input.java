2f48dae9335f135cfe0448a4ef51b55339e91873
JunoJin <jjin525@aucklanduni.ac.nz>
2017-08-21 17:24:59 +1200

    forgot input.java

--- src/main/java/main/Input.java
+++ src/main/java/main/Input.java
@@ -1,5 +1,6 @@
 package main;
 
+import org.gephi.datalab.api.AttributeColumnsController;
 import org.gephi.filters.api.FilterController;
 import org.gephi.filters.api.Query;
 import org.gephi.filters.plugin.attribute.AttributeEqualBuilder;
@@ -12,11 +13,11 @@ import org.gephi.project.api.ProjectController;
 import org.gephi.project.api.Workspace;
 import org.openide.util.Lookup;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.math.RoundingMode;
+import java.sql.Timestamp;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -55,58 +56,104 @@ public class Input {
         return graph;
     }
 
-    public UndirectedGraph getZoneGraph(UndirectedGraph completeGraph, String zoneType){
-        Column col = completeGraph.getModel().getNodeTable().getColumn("Label");
-        FilterController filterController = Lookup.getDefault().lookup(FilterController.class);
+//    public UndirectedGraph getZoneGraph(UndirectedGraph completeGraph, String zoneType){
+//        Column col = completeGraph.getModel().getNodeTable().getColumn("Label");
+//        FilterController filterController = Lookup.getDefault().lookup(FilterController.class);
+//
+//        AttributeEqualBuilder.EqualStringFilter equalStringFilter = new AttributeEqualBuilder.EqualStringFilter.Node(col);
+//        equalStringFilter.init(completeGraph);
+//        equalStringFilter.setUseRegex(true);
+//        equalStringFilter.setPattern("^.*(" + zoneType + ").*$");
+//        Query query = filterController.createQuery(equalStringFilter);
+//        GraphView graphView = filterController.filter(query);
+//
+//        return completeGraph.getModel().getUndirectedGraph(graphView);
+//    }
 
-        AttributeEqualBuilder.EqualStringFilter equalStringFilter = new AttributeEqualBuilder.EqualStringFilter.Node(col);
-        equalStringFilter.init(completeGraph);
-        equalStringFilter.setUseRegex(true);
-        equalStringFilter.setPattern("^.*(" + zoneType + ").*$");
-        Query query = filterController.createQuery(equalStringFilter);
-        GraphView graphView = filterController.filter(query);
+    public void setXY(UndirectedGraph graph){
+        BufferedReader br = null;
+        String line;
+        String splitRegex = ",";
+        boolean headerRecorded = false;
 
-        return completeGraph.getModel().getUndirectedGraph(graphView);
+        DecimalFormat df = new DecimalFormat("###.#####");
+        df.setRoundingMode(RoundingMode.DOWN);
+
+        try {
+            for (Node n:graph.getNodes()) {
+                String[] nodeData = n.getLabel().split( ";");
+                br = new BufferedReader(new FileReader("src\\main\\java\\main\\YX.csv"));
+                while ((line = br.readLine()) != null) {
+                    if(!headerRecorded){
+                        headerRecorded = true;
+                        continue;
+                    }
+
+                    String[] lineData = line.split(splitRegex);
+
+                    float x = Float.parseFloat(lineData[1]);
+                    float y = Float.parseFloat(lineData[0]);
+
+                    if(nodeData[0].equals(lineData[3]) &&
+                            nodeData[1].equals(lineData[4]) &&
+                            nodeData[2].equals(lineData[5]) &&
+                            nodeData[3].equals(lineData[6]) &&
+                            Math.abs(Double.parseDouble(nodeData[4]) - Double.parseDouble(lineData[7])) < 0.001 &&
+                            Math.abs(Double.parseDouble(nodeData[5]) - Double.parseDouble(lineData[8])) < 0.001){
+                        n.setPosition(x,y);
+                    }
+                }
+                headerRecorded = false;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
-    
-    public NodeListHolder  fileToNodeListHolder(String fileName){
-    	NodeListHolder nlh = new NodeListHolder();
-    	List<PopNode> resNodes = new ArrayList<PopNode>();
-    	List<FacNode> facNodes = new ArrayList<FacNode>();
-    	String line;
-    	String splitRegex = ",";
-    	boolean headerRecorded = false;
-    	
-    	try {
-			BufferedReader br = new BufferedReader(new FileReader(fileName));
-			while((line=br.readLine())!=null){
-				if(!headerRecorded){
-					headerRecorded = true;
-					continue;
-				}
-				
-				String[] lineData = line.split(splitRegex);
-				if(lineData[4].startsWith("Business")){
-					facNodes.add(new FacNode(Float.valueOf(lineData[0]),Float.valueOf(lineData[1]),Integer.parseInt(lineData[2])));
-				}
-				else if(lineData[4].startsWith("Residential")){
-					resNodes.add(new PopNode(Float.valueOf(lineData[0]),Float.valueOf(lineData[1]),Integer.parseInt(lineData[2]),lineData[5], Float.valueOf(lineData[8])));
-				}
-			}
-    	
-    	
-    	} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}    
-    	nlh.setNodeLists(resNodes,facNodes);
-    	
-    	
-		return nlh;
-    	
-    }
+//
+//    public NodeListHolder  fileToNodeListHolder(String fileName){
+//    	NodeListHolder nlh = new NodeListHolder();
+//    	List<PopNode> resNodes = new ArrayList<PopNode>();
+//    	List<FacNode> facNodes = new ArrayList<FacNode>();
+//    	String line;
+//    	String splitRegex = ",";
+//    	boolean headerRecorded = false;
+//
+//    	try {
+//			BufferedReader br = new BufferedReader(new FileReader(fileName));
+//			while((line=br.readLine())!=null){
+//				if(!headerRecorded){
+//					headerRecorded = true;
+//					continue;
+//				}
+//
+//				String[] lineData = line.split(splitRegex);
+//				if(lineData[4].startsWith("Business")){
+//					facNodes.add(new FacNode(Float.valueOf(lineData[0]),Float.valueOf(lineData[1]),Integer.parseInt(lineData[2])));
+//				}
+//				else if(lineData[4].startsWith("Residential")){
+//					resNodes.add(new PopNode(Float.valueOf(lineData[0]),Float.valueOf(lineData[1]),Integer.parseInt(lineData[2]),lineData[5], Float.valueOf(lineData[8])));
+//				}
+//			}
+//
+//
+//    	} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//    	nlh.setNodeLists(resNodes,facNodes);
+//
+//
+//		return nlh;
+//
+//    }
 /*
- * 
+ *
     public NodeListHolder graphToNodeListHolder(UndirectedGraph graph){
         NodeListHolder nlh = new NodeListHolder();
 
@@ -129,22 +176,4 @@ public class Input {
         return nlh;
     }
 */
-    class NodeListHolder{
-        private List<PopNode> popNodeList = new ArrayList<>();
-        private List<FacNode> facNodeList = new ArrayList<>();
-
-        private void setNodeLists(List<PopNode> popNodeList, List<FacNode> facNodeList){
-            this.popNodeList = popNodeList;
-            this.facNodeList = facNodeList;
-        }
-
-        public List<PopNode> getPopNodeList() {
-            return popNodeList;
-        }
-
-        public List<FacNode> getFacNodeList() {
-            return facNodeList;
-        }
-    }
-
 }
