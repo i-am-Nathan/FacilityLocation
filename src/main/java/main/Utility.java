6b148b3641c3e637bd5b89dbfdf35621c1aed093
JunoJin <jjin525@aucklanduni.ac.nz>
2017-09-06 00:26:53 +1200

    now separated modularity and centrality

--- src/main/java/main/Utility.java
+++ src/main/java/main/Utility.java
@@ -62,10 +62,8 @@ public class Utility {
 
 	}
 
-	//Find k facilities based on the clustering and centrality of the node population
-	static List<List<Node>> findInitialK(Graph graph, int k, float coverageThreshold, String centralityType) {
-		List<List<Node>> nodeLists = new ArrayList<>();
-		List<org.gephi.graph.api.Node> kNodeList = new ArrayList<>();
+	static List<Subgraph> findModularityClasses(Graph graph, int k, float coverageThreshold){
+		List<Subgraph> communitySubgraphs = new ArrayList<>();
 
 		FilterController filterController = Lookup.getDefault().lookup(FilterController.class);
 
@@ -78,12 +76,6 @@ public class Utility {
 		AppearanceModel appearanceModel = appearanceController.getModel();
 
 		UndirectedGraph cleanGraph = graph.getModel().getUndirectedGraph(graphView);
-
-		AttributeColumnsController attributeColumnsController = Lookup.getDefault()
-				.lookup(AttributeColumnsController.class);
-		Table edgeTable = cleanGraph.getModel().getEdgeTable();
-		attributeColumnsController.copyColumnDataToOtherColumn(edgeTable, edgeTable.getColumn("Label"),
-				edgeTable.getColumn("Weight"));
 
 		Modularity modularity = new Modularity();
 		modularity.setUseWeight(true);
@@ -119,34 +111,48 @@ public class Utility {
 
 			}
 
-//			System.out.println(partition2.size() + " communities found");
+			System.out.println(partition2.size() + " communities found");
 			percentages = partition2.getSortedValues().toArray();
 
 			for (int classIndex = 0; classIndex < k; classIndex++) {
-//				System.out.println("Community " + classIndex + ": " + partition2.percentage(percentages[classIndex]));
+				System.out.println("Community " + classIndex + ": " + partition2.percentage(percentages[classIndex]));
 				totalCoverage += partition2.percentage(percentages[classIndex]);
 			}
 			resolution++;
-//			System.out.println("Total coverage over " + k + " communities: " + totalCoverage + "%\n");
+			System.out.println("Total coverage over " + k + " communities: " + totalCoverage + "%\n");
 		}
-
-//		System.out.println("#### Total coverage now over " + coverageThreshold + "%!\n\n");
 
 		PartitionBuilder.PartitionFilter partitionFilter = new PartitionBuilder.NodePartitionFilter(classColumn,
 				appearanceModel);
 		for (int classIndex = 0; classIndex < k; classIndex++) {
-			List<Node> communityNodeList = new ArrayList<>();
 			partitionFilter.addPart(percentages[classIndex]);
 			Query query1 = filterController.createQuery(partitionFilter);
 			filterController.setSubQuery(query1, query);
 			GraphView communityView = filterController.filter(query1);
-			UndirectedGraph communityGraph = graph.getModel().getUndirectedGraph(communityView);
+			communitySubgraphs.add(graph.getModel().getGraph(communityView));
+			partitionFilter.removePart(percentages[classIndex]);
+		}
 
+
+		return communitySubgraphs;
+	}
+
+	//Find k facilities based on the clustering and centrality of the node population
+	static List<List<Node>> findInitialK(Graph graph, int k, float coverageThreshold, String centralityType) {
+		List<List<Node>> nodeLists = new ArrayList<>();
+		List<Node> kNodeList = new ArrayList<>();
+		List<Subgraph> communitySubgraphs = findModularityClasses(graph, k, coverageThreshold);
+
+		for(Subgraph sg: communitySubgraphs){
+		    System.out.println(sg.getNodeCount());
+        }
+
+		for(Subgraph sg: communitySubgraphs){
 			EigenvectorCentrality eigenvectorCentrality = new EigenvectorCentrality();
-			eigenvectorCentrality.execute(communityGraph);
+			eigenvectorCentrality.execute(sg);
 
 			GraphDistance graphDistance = new GraphDistance();
-			graphDistance.execute(communityGraph);
+			graphDistance.execute(sg);
 			double maxCentrality = 0;
 			org.gephi.graph.api.Node maxCentralityNode = null;
 
@@ -155,31 +161,31 @@ public class Utility {
 
 			Column centralityColumn = null;
 
-			for (org.gephi.graph.api.Node node : communityGraph.getNodes()) {
-				communityNodeList.add(node);
-				if (!node.getLabel().contains(FACILITY_NAME))
-					continue;
-				for (Column col : node.getAttributeColumns()) {
-					if (col.getTitle().equals(centralityType)) {
-						centralityColumn = col;
-						break;
+			for (org.gephi.graph.api.Node node : sg.getNodes()) {
+				if (node.getLabel().contains(FACILITY_NAME)) {
+					for (Column col : node.getAttributeColumns()) {
+						if (col.getTitle().equals(centralityType)) {
+							centralityColumn = col;
+							break;
+						}
 					}
-				}
-				double nodeCentrality = (double) node.getAttribute(centralityColumn);
-				if (nodeCentrality > maxCentrality) {
-					maxCentralityNode = node;
-					maxCentrality = nodeCentrality;
+					double nodeCentrality = (double) node.getAttribute(centralityColumn);
+					if (nodeCentrality > maxCentrality) {
+						maxCentralityNode = node;
+						maxCentrality = nodeCentrality;
+					}
 				}
 			}
 			kNodeList.add(maxCentralityNode);
-			nodeLists.add(communityNodeList);
-			partitionFilter.removePart(percentages[classIndex]);
+			Iterable<Node> communityIterable = sg.getNodes();
+			List<Node> communityNodes = new ArrayList<>();
+			communityIterable.forEach(communityNodes::add);
+			nodeLists.add(communityNodes);
 		}
 
 		nodeLists.add(kNodeList);
 
 		return nodeLists;
-
 	}
 	
 	//Uses dijkstra algorithm and finds the distance of the whole graph relative to the node
