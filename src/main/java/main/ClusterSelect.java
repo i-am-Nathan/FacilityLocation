bb43a98b322f24263494b45cfcd8711dbfa5695d
JunoJin <jjin525@aucklanduni.ac.nz>
2017-09-06 03:45:07 +1200

    fixed clusterselect, now explicitly calculates score

--- src/main/java/main/ClusterSelect.java
+++ src/main/java/main/ClusterSelect.java
@@ -1,36 +1,59 @@
 package main;
 
+import org.gephi.algorithms.shortestpath.DijkstraShortestPathAlgorithm;
 import org.gephi.graph.api.Graph;
 import org.gephi.graph.api.Node;
 import org.gephi.graph.api.Subgraph;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class ClusterSelect {
 
-    public List<Node> Search(Graph graph, int facCount, float coverageThreshold, boolean useEuclidean){
+    public List<List<Node>> Search(Graph graph, int facCount, float coverageThreshold, boolean useEuclidean){
         List<Subgraph> communitySubgraphs = Utility.findModularityClasses(graph, facCount, coverageThreshold);
+        List<List<Node>> nodeLists = new ArrayList<>();
 
         List<Node> bestNodes = new ArrayList<>();
 
         for(Subgraph communitySubgraph: communitySubgraphs){
+            List<Node> communityNodes = new ArrayList<>();
             double bestCost = Double.MAX_VALUE;
             Node currentBestNode = null;
+
             for(Node n: communitySubgraph.getNodes()){
+                communityNodes.add(n);
                 if(n.getLabel().contains(Utility.FACILITY_NAME)){
-                    List<Node> tempList = Arrays.asList(n);
-                    double tempCost = Utility.calculateFinalScore(communitySubgraph, tempList, useEuclidean);
+                    double tempCost = 0;
+                    HashMap<Node, Double> distances = Utility.computeDistances(graph, n);
+
+                    for(Node targetNode: communitySubgraph.getNodes()){
+                        String[] nodeLabels = targetNode.getLabel().split(";");
+                        if(nodeLabels[3].contains(Utility.RESIDENTIAL_NAME)){
+                            double popScore = Utility.CalculatePopulationScore(nodeLabels[2], Float.valueOf(nodeLabels[5]));
+                            double distance = distances.get(targetNode);
+                            if(Double.isFinite(distance) && Double.isFinite(popScore))
+                                tempCost += popScore * distance;
+
+                        }
+                    }
+
                     if(tempCost < bestCost ){
                         bestCost = tempCost;
+                        System.out.printf("New Best cost: %f\n", bestCost);
                         currentBestNode = n;
                     }
                 }
             }
             bestNodes.add(currentBestNode);
+            nodeLists.add(communityNodes);
+
+            System.out.println("#######");
         }
 
-        return bestNodes;
+        nodeLists.add(bestNodes);
+
+        return nodeLists;
     }
 }
